7c46ec413fc97ef310a50d73ee71af0b8db75a84
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2025-04-26 11:43:10 +0530

    added multer cloudinary and database connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv"
 import connectDb from "./db/index.js"
+import {app} from "app.js"
 
 dotenv.config({
   path: './env'
 })
 
 connectDb()
+.then(()=>{
+  app.lister(process.env.PORT || 8000 , ()=>{
+    console.log(`Server is running at port ${process.env.PORT}`)
+  })
+})
+.catch(()=>{
+  console.log("MongoDB connection failed !!!")
+})
