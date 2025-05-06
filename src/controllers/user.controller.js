7c9642160430c523dec10c947c89c66a7616f29c
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2025-05-06 13:01:39 +0530

    controller for user channel profile using aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -368,6 +368,75 @@ const updateCoverImage = asyncHandler(async(req,res)=>{
     )
 })
 
+const userChannelProfile = asyncHandler(async(req,res)=>{
+    const {username} = req.params
+    if(!username.trim()){
+        throw new apiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount: {
+                    $size: "$subscribers"
+                },
+                channelSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                email: 1,
+                avatar:1,
+                coverImage: 1,
+                subscriberCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new apiError(404, "Channel does not exist")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(200, channel[0], "User channel fetched successfully")
+    )
+})
 
 
 
@@ -383,5 +452,6 @@ export {
     updateAccountDetail,
     updateUserAvatar,
     updateCoverImage,
+    userChannelProfile,
 
 }
