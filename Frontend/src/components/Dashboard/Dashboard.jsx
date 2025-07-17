4619706f64e6eb7aa40e41aa3d9e6df437d570ac
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2025-07-17 18:17:10 +0530

    feat: Enhance Dashboard with Sidebar integration and improved video loading handling

--- Frontend/src/components/Dashboard/Dashboard.jsx
+++ Frontend/src/components/Dashboard/Dashboard.jsx
@@ -8,18 +8,36 @@ import {
   useNavigate,
 } from "react-router-dom";
 import Navbar from "../Navbar";
-import { VideoContext } from "../context/UserVideoContext";
+import {  VideoContext } from "../context/VideoContext";
 import useFilteredSortedVideos from "../hooks/useFilteredSortedVideos";
 import useRandomVideo from "../hooks/useRandomVideo";
+import Sidebar from "./Sidebar";
+import useSubscription from "../hooks/useSubscription";
+import useUserPlaylist from "../hooks/useUserPlaylist";
 
 const Dashboard = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const { videos, loading, error } = useRandomVideo();
+  const {subscribedChannels} = useSubscription()
+  const {playlists} = useUserPlaylist()
+  // console.log("Playlist on profile", playlists)
+
   const [searchQuery, setSearchQuery] = useState("");
 
-  const [subscribedChannels, setSubscribedChannel] = useState([]);
+  // const [subscribedChannels, setSubscribedChannel] = useState([]);
   const [logedin, setLogedin] = useState(false);
+  const [isSidebarVisible, setSidebarVisible] = useState(true)
+
+  const toggleSidebar1 = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
+
+  const [isSidebarCollapsed, setSidebarCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarCollapsed((prev) => !prev);
+  };
 
   useEffect(() => {
     getSubscribedChannel();
@@ -75,196 +93,41 @@ const Dashboard = () => {
       });
   };
 
-  const handleChannelClick = (channel) => {
-    navigate(`/dashboard/${channel.username}`);
-  };
-
-  const handleLogout = () => {
-    logout();
-  };
+  
 
   const { filteredVideos, handleSort, sortKey, sortOrder } =
     useFilteredSortedVideos(videos, searchQuery);
 
   return (
-    <VideoContext.Provider value={{filteredVideos}}>
-      <div className="flex flex-col">
+    <VideoContext.Provider value={{ filteredVideos, subscribedChannels, playlists }}>
+      <div className="flex flex-col h-screen">
+        {/* Navbar */}
         <div className="bg-black shadow-2xl">
-          <Navbar onSearch={setSearchQuery} />
+          <Navbar onSearch={setSearchQuery} onToggleSidebar={toggleSidebar}/>
         </div>
-        <div className="flex ">
-          <div className="w-[16%] bg-black h-lvh  overflow-y-auto  scrollbar-hover pb-40 ">
-            <div className="flex flex-col justify-center  pt-6 px-2  text-[#ece9e9] font-medium">
-              <Link
-                to={"/dashboard/home"}
-                className={`${
-                  location.pathname === "/dashboard/home"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-solid fa-house mr-5"></i> Home
-              </Link>
-              <Link
-                to={"/dashboard/short"}
-                className={`${
-                  location.pathname === "/dashboard/short"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-solid fa-bolt mr-5"></i> Shorts
-              </Link>
-              <Link
-                to={"/dashboard/Subscriptions"}
-                className={`${
-                  location.pathname === "/dashboard/Subscriptions"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-solid fa-tv mr-5"></i> Subscriptions
-              </Link>
 
-              <hr className="text-[#3b3b3b] w-[100%] m-3" />
+        {/* Sidebar + Content */}
+        <div className="flex flex-1 overflow-hidden">
+          {/* Sidebar */}
 
-              <Link
-                to={"/dashboard/Subscriptions"}
-                className={`${
-                  location.pathname === "/dashboard/Subscriptions"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                You<i class="fa-solid fa-angle-right ml-4"></i>
-              </Link>
+          <div
+          className={`${
+            isSidebarCollapsed ? "w-20" : "w-64"
+          } bg-black overflow-y-auto scrollbar-hover pb-40 text-[#ece9e9]  `}
+        >
+          <Sidebar isCollapsed={isSidebarCollapsed} />
+        </div>
 
-              <Link
-                to={"/dashboard/my-video"}
-                className={`${
-                  location.pathname === "/dashboard/my-video"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-solid fa-clock-rotate-left mr-5"></i> History
-              </Link>
-              <Link
-                to={"/dashboard/my-video"}
-                className={`${
-                  location.pathname === "/dashboard/my-video"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-solid fa-list mr-5"></i> Playlist
-              </Link>
-
-              <Link
-                to={"/dashboard/my-video"}
-                className={`${
-                  location.pathname === "/dashboard/my-video"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-solid fa-video mr-5"></i> your Video
-              </Link>
-
-              <Link
-                to={"/dashboard/my-video"}
-                className={`${
-                  location.pathname === "/dashboard/my-video"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-solid fa-graduation-cap mr-5"></i> your Cources
-              </Link>
-
-              <Link
-                to={"/dashboard/my-video"}
-                className={`${
-                  location.pathname === "/dashboard/my-video"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-regular fa-clock mr-5"></i> Watch Later
-              </Link>
-
-              <Link
-                to={"/dashboard/my-video"}
-                className={`${
-                  location.pathname === "/dashboard/my-video"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-regular fa-thumbs-up mr-5"></i> Liked Videos
-              </Link>
-
-              <Link
-                to={"/dashboard/upload-video"}
-                className={`${
-                  location.pathname === "/dashboard/upload-video"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-solid fa-upload mr-5"></i> Upload
-              </Link>
-
-              <Link
-                // to={"/dashboard/logout"}
-                onClick={handleLogout}
-                className={`${
-                  location.pathname === "/dashboard/logout"
-                    ? "bg-[#3b3b3b] "
-                    : "hover:bg-[#3b3b3b]"
-                } w-[90%] py-2 px-4 duration-300 rounded-xl`}
-              >
-                <i class="fa-solid fa-right-from-bracket mr-5"></i> Logout
-              </Link>
-
-              <hr className="text-[#3b3b3b] w-[100%] m-3" />
-              <h2 className="mx-4 my-1">Subscriptions</h2>
-
-              <div>
-                {subscribedChannels.map((subscribedChannel) => (
-                  <div
-                    onClick={() => {
-                      handleChannelClick(subscribedChannel.channel);
-                    }}
-                    className="flex flex-row items-center hover:bg-[#3b3b3b] w-[90%] py-2 duration-300 rounded-xl cursor-pointer"
-                    key={subscribedChannel._id}
-                  >
-                    <img
-                      onClick={() => {
-                        handleChannelClick(subscribedChannel.channel);
-                      }}
-                      className="h-7 w-7 object-cover rounded-xl  mx-5"
-                      src={subscribedChannel.channel.avatar}
-                      alt="avatar"
-                    />
-                    <h2
-                      onClick={() => {
-                        handleChannelClick(subscribedChannel.channel);
-                      }}
-                    >
-                      {subscribedChannel.channel.fullName}
-                    </h2>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-
-          <div className="w-[84%] bg-black  h-lvh  overflow-y-auto scrollbar-hover">
+          {/* Main Content */}
+          <div className="flex-1 bg-black overflow-y-auto scrollbar-hover">
             {loading ? (
-              <div className="p-4 flex justify-center text-white">Loading videos...</div>
+              <div className="p-4 text-white text-center">
+                Loading videos...
+              </div>
             ) : error ? (
-              <div className="p-4 text-red-400 ">Error loading videos.</div>
+              <div className="p-4 text-red-400 text-center">
+                Error loading videos.
+              </div>
             ) : (
               <Outlet />
             )}
