7b4d4784f174db6f84c71f6c7e0cfcd8d636ce42
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2025-07-18 20:00:56 +0530

    fix: Adjust padding in Home component for improved layout

--- Frontend/src/components/Dashboard/Home.jsx
+++ Frontend/src/components/Dashboard/Home.jsx
@@ -3,7 +3,7 @@ import Video from "./Video";
 const Home = () => {
 
   return (
-    <div className="grid grid-cols-3 gap-x-2 h-fit items-start p-5 pb-40">
+    <div className="grid grid-cols-3 gap-x-2 h-fit items-start py-5 px-2 pb-40">
       {<Video />}
     </div>
   );
